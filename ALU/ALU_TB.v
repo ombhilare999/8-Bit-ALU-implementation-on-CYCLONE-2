d21b511ae1f5e1b1f8cc1a421e22f7852f4f155a
ombhilare999 <ombhilare999@gmail.com>
2020-10-28 13:51:31 +0530

    Testbench code updated

--- ALU/ALU_TB.v
+++ ALU/ALU_TB.v
@@ -27,14 +27,14 @@ ALU i1
 initial 
 begin
     $monitor($time, "  a = %b b = %b cin = %b ::  out = %b  cout = %b  :: mode = %b control_word = %b", a, b, C_IN, OUT, C_OUT, MODE_SELECT, CONTROL_LINE);
-    $dumpfile("a.vcd");
-    $dumpvars(0, ALU_TB);
+    //$dumpfile("a.vcd");
+    //$dumpvars(0, ALU_TB);
 end
 
 initial
 begin
     a = 8'd2; b = 8'd3; C_IN = 0;
-    MODE_SELECT = 1;
+    MODE_SELECT = 0;
     CONTROL_LINE = 3'd0;
     for(i = 0; i < 2; i++)
     begin
